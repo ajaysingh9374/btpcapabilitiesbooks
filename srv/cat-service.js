a03abe1f498a15b654f37188b069bc852a9b803a
Ajay Singh <ajaysingh9374@hotmail.com>
2023-08-10 11:52:25 +0000

    first commit

---
+++ srv/cat-service.js
@@ -0,0 +1,12 @@
+const cds = require('@sap/cds')
+class CatalogService extends cds.ApplicationService {
+    async init() {
+        const { Books } = this.entities
+        this.before('READ', Books, each => {
+            if (each.stock < 20) each.title += ' (only a few left!)'
+        })
+        await super.init()
+    }
+}
+
+module.exports = CatalogService
